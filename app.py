f7b23ea57e6283607cc2d826114c5a1613bc4ffe
Vasile Cosmin <92382573+VasileCosmin@users.noreply.github.com>
2023-01-29 16:42:49 +0200

    Added message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, flash, render_template, session, redirect, request
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -19,133 +19,146 @@ db = SQL("sqlite:///users.db")
 @app.route("/add", methods=["POST"])
 @login_required
 def add():
-  if request.method == 'POST':
-    user_id = session["user_id"]
-    name = request.form.get("name")
-    equipment = request.form.get("equipment")
-    instructions = request.form.get("instructions")
+    if request.method == "POST":
+        user_id = session["user_id"]
+        name = request.form.get("name")
+        equipment = request.form.get("equipment")
+        instructions = request.form.get("instructions")
 
-    db.execute("INSERT INTO exercises (user_id, exercise, equipment, instructions) VALUES (?, ?, ?, ?)", user_id, name, equipment, instructions)
+        db.execute(
+            "INSERT INTO exercises (user_id, exercise, equipment, instructions) VALUES (?, ?, ?, ?)",
+            user_id,
+            name,
+            equipment,
+            instructions,
+        )
 
-    return redirect("/workout")
+        flash("Exercise Added!")
+
+        return redirect("/exercises")
 
 
 @app.route("/delete", methods=["POST"])
 @login_required
 def delete():
-  user_id = session["user_id"]
-  exercise = request.form.get("exercise")
-  if id:
-    db.execute("DELETE FROM exercises WHERE exercise = ? AND user_id = ?", exercise, user_id)
-  
-  return redirect("/workout")
+    user_id = session["user_id"]
+    exercise = request.form.get("exercise")
+    if id:
+        db.execute(
+            "DELETE FROM exercises WHERE exercise = ? AND user_id = ?",
+            exercise,
+            user_id,
+        )
+
+    return redirect("/workout")
 
 
 @app.route("/workout")
 @login_required
 def workout():
-  user_id = session["user_id"]
-  exercises = db.execute("SELECT * FROM exercises WHERE user_id = ?", user_id)
-  
-  return render_template("workout.html", exercises=exercises)
+    user_id = session["user_id"]
+    exercises = db.execute("SELECT * FROM exercises WHERE user_id = ?", user_id)
+
+    return render_template("workout.html", exercises=exercises)
 
 
 @app.route("/")
 @login_required
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/info")
 @login_required
 def info():
-  return render_template("info.html")
+    return render_template("info.html")
 
 
-@app.route("/exercises", methods=['GET', 'POST'])
+@app.route("/exercises", methods=["GET", "POST"])
 @login_required
 def exercises():
-  if request.method == 'POST':
-    muscle = request.form.get("muscles")
-    exercises = get_exercises(muscle)
+    if request.method == "POST":
+        muscle = request.form.get("muscles")
+        exercises = get_exercises(muscle)
+
+        return render_template("exercises.html", exercises=exercises)
+    else:
+        return render_template("exercises.html")
 
 
-    return render_template("exercises.html", exercises=exercises)
-  else:
-    return render_template("exercises.html")
-
-
-@app.route("/login", methods=['POST', 'GET'])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-  session.clear()
+    session.clear()
 
-  if request.method == 'POST':
-    username = request.form.get("username")
-    password = request.form.get("password")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    if not username:
-      error = 'Enter Username!'
-      return render_template("login.html", error1=error)
-    elif not password:
-      error = 'Enter Password!'
-      return render_template("login.html", error2=error)
+        if not username:
+            error = "Enter Username!"
+            return render_template("login.html", error1=error)
+        elif not password:
+            error = "Enter Password!"
+            return render_template("login.html", error2=error)
 
-    rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
 
-    if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-      error = "Invalid Username And/Or Password"
-      return render_template("login.html", error1 = error)
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+            error = "Invalid Username And/Or Password"
+            return render_template("login.html", error1=error)
 
-    session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["id"]
 
-    return redirect("/")
+        return redirect("/")
 
-  else:
-    return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-  if request.method == 'POST':
-    username = request.form.get("username")
-    password = request.form.get("password")
-    confirmation = request.form.get("confirmation")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
 
-    if not username:
-      error = 'Enter Username!'
-      return render_template("register.html", error=error)
-    elif not password:
-      error = 'Enter Password!'
-      return render_template("register.html", error_pass=error)
-    elif not confirmation:
-      error= 'Enter Password Again!'
-      return render_template("register.html", error_conf=error)
+        if not username:
+            error = "Enter Username!"
+            return render_template("register.html", error=error)
+        elif not password:
+            error = "Enter Password!"
+            return render_template("register.html", error_pass=error)
+        elif not confirmation:
+            error = "Enter Password Again!"
+            return render_template("register.html", error_conf=error)
 
-    if confirmation != password:
-      error = 'Passwords did not match'
-      return render_template("register.html", error_conf=error)
+        if confirmation != password:
+            error = "Passwords did not match"
+            return render_template("register.html", error_conf=error)
 
-    hash = generate_password_hash(password, 'pbkdf2:sha256', 2)
+        hash = generate_password_hash(password, "pbkdf2:sha256", 2)
 
-    usernames = db.execute("SELECT username FROM users")
+        usernames = db.execute("SELECT username FROM users")
 
-    for i in range(len(usernames)): 
-      if username == usernames[i]:
-        error = 'Username Already Exists!'
-        return render_template("register.html", error=error)
+        for i in range(len(usernames)):
+            if username == usernames[i]:
+                error = "Username Already Exists!"
+                return render_template("register.html", error=error)
 
-    new_user = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash) 
+        new_user = db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)", username, hash
+        )
 
-    session["user_id"] = new_user
+        session["user_id"] = new_user
 
-    return redirect("/")
-  else:
-    return render_template("register.html")
+        return redirect("/")
+    else:
+        return render_template("register.html")
 
 
 @app.route("/signout")
 @login_required
 def signout():
-  session.clear()
+    session.clear()
 
-  return redirect("/login")
+    return redirect("/login")
